537f690bdae882951bb8bd523611cae1754ffe18
Maciek Stopa <maciej.stopa@techmo.pl>
2023-02-22 20:57:01 +0100

    Add argparse

--- currency_converter.py
+++ currency_converter.py
@@ -1,58 +1,25 @@
+import argparse
+
 import requests
 
-CURRENCIES = ("USD", "EUR", "CHF", "GBP", "NOK")
-
-
 def get_rate(code: str) -> float:
-    url = f"http://api.nbp.pl/api/exchangerates/ratezzz/a/{code}"
+    url = f"http://api.nbp.pl/api/exchangerates/rates/a/{code}"
     response = requests.get(url)
     if response.status_code == 200: 
         rate = response.json()
         return rate['rates'][0]['mid']
     else:
-        raise Exception("Invalid currency code")
+        raise ValueError("Invalid currency code")
 
-
-def fetch_input_amount():
-    input_str = input("Enter an amout of money: \n")
-    
-    input_str = input_str.replace(",", ".")
-    try:
-        input_amount = float(input_str)
-        return input_amount
-    except ValueError:
-        print("Invalid input data!\n")
-        return 
-
-
-def fetch_currency_code() -> str:
-    input_code = input("Enter an currency_code: \n[USD, EUR, CHF, GBP, NOK] \n")
-
-    if input_code not in CURRENCIES:
-        raise ValueError("Invalid currency code!")
-
-    return input_code
-
-
-def currency_converter():
-    amount = fetch_input_amount()
-    print("\n")
-    if amount == None:
-        return
-
-    rate_code = fetch_currency_code()
-    if rate_code == None:
-        return
-
-    rate = get_rate(rate_code)
-    if rate == None:
-        return
-
-    result = amount * rate
-    result = round(result, 2)
-    print("\n")
-    print(str(result) + ' PLN (Currency rate: ' + str(rate) + ')')    
-
+def convert(amount: float, currency_code: str) -> None:
+    rate = get_rate(currency_code)
+    return round(amount * rate, 2)
 
 if __name__ == "__main__":
-    currency_converter()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("amount", type=float, help="Amount of money")
+    parser.add_argument("currency_code", type=str, help="Currency code")
+    args = parser.parse_args()
+
+    result = convert(args.amount, args.currency_code)
+    print(result)
